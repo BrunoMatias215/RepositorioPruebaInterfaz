c38d0122e048f1ef3e0feaca0c08f841e8932b37
Bruno <brunomatiasleto@hotmail.com>
2020-10-02 21:11:18 -0300

    Yoel3

--- WindowsFormsApp1/WindowsFormsApp1/IntefazGeneral.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/IntefazGeneral.Designer.cs
@@ -107,7 +107,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Yoel2";
+            this.label1.Text = "Yoel3";
             // 
             // PanelInterfaz
             // 
