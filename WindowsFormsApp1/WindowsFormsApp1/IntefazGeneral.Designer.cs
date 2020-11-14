66a556eb48c87ccf1a5fc74e4c6d880c2b777e80
Bruno <brunomatiasleto@hotmail.com>
2020-11-14 12:02:40 -0300

    Base de Datos

--- WindowsFormsApp1/WindowsFormsApp1/IntefazGeneral.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/IntefazGeneral.Designer.cs
@@ -108,9 +108,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(32, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre de Usuario";
+            this.label1.Text = "Nombre de Usuario [Rol]";
             // 
             // PanelInterfaz
             // 
